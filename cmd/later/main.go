2706f1afeb52c98aa35d71339ffcfb7f0eeccf9e
Lucas Gregory <lrg006@bucknell.edu>
2020-02-18 23:56:47 -0800

    client to database integration

--- cmd/later/main.go
+++ cmd/later/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
-	"later.co/pkg/server/user"
+	"later.co/pkg/repository"
+
+	userrepo "later.co/pkg/repository/user"
+
+	userserver "later.co/pkg/server/user"
 )
 
 func main() {
 	router := gin.Default()
 
-	user.RegisterEndpoints(router)
+	db, err := repository.InitDB()
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	userrepo.DB = db
+
+	userserver.RegisterEndpoints(router)
 
 	router.Run(":8000")
 }
