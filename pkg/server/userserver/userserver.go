54faf3b763139c7fc75be039d11f21fe9bc3ef16
Lucas Gregory <lrg006@bucknell.edu>
2020-02-23 11:10:52 -0800

    update users

--- pkg/server/userserver/userserver.go
+++ pkg/server/userserver/userserver.go
@@ -1,9 +1,8 @@
 package userserver
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -15,7 +14,9 @@ import (
 // RegisterEndpoints defines handlers for endpoints for the user service
 func RegisterEndpoints(router *gin.Engine) {
 	router.POST("/users/sign-up", signUp)
+
 	router.GET("/users/by-id", byID)
+	router.GET("/users/all", allUsers)
 }
 
 func signUp(context *gin.Context) {
@@ -53,30 +54,51 @@ func byID(context *gin.Context) {
 	id := context.Query("id")
 
 	if id == "" {
-		context.String(http.StatusBadRequest, "Parameter id is required\n")
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Parameter id is required"})
 		return
 	}
 
 	idAsUUID, err := uuid.Parse(id)
 
 	if err != nil {
-		context.String(http.StatusBadRequest, "Parameter id must be a uuid\n")
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Parameter id must be a uuid"})
 		return
 	}
 
 	user, err := userrepo.ByID(idAsUUID)
 
 	if err != nil {
-		fmt.Println(err)
-		context.String(http.StatusNotFound, "User does not exist\n")
+		context.JSON(http.StatusNotFound, gin.H{"error": "User does not exist"})
 		return
 	}
 
-	response, err := json.Marshal(user)
+	context.JSON(http.StatusOK, user)
+}
 
-	if err != nil {
-		context.String(http.StatusInternalServerError, "Error converting object into json")
+func allUsers(context *gin.Context) {
+	limit := context.Query("limit")
+
+	var err error
+	var limitint int
+
+	if limit == "" {
+		limitint = 100
+	} else {
+		limitint, err = strconv.Atoi(limit)
 	}
 
-	context.String(http.StatusOK, string(response))
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Parameter limit must be a number"})
+		return
+	}
+
+	users, err := userrepo.All(limitint)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, users)
+
 }
