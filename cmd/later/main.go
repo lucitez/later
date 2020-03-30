86f1cafa6cc4e06f681aeffe3ba8ddcdea70a5f1
Lucas Gregory <lucgregory1@gmail.com>
2020-03-29 19:27:38 -0700

    refactor structure

--- cmd/later/main.go
+++ cmd/later/main.go
@@ -4,7 +4,8 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
-	"later.co/pkg/repository/util"
+
+	"later/pkg/repository/util"
 )
 
 func main() {
@@ -17,20 +18,20 @@ func main() {
 	}
 
 	contentServer := InitializeContent(db)
-	domainServer := InitializeDomain(db)
-	friendServer := InitializeFriend(db)
-	friendRequestServer := InitializeFriendRequest(db)
-	shareServer := InitializeShare(db)
-	userContentServer := InitializeUserContent(db)
-	userServer := InitializeUser(db)
+	// domainServer := InitializeDomain(db)
+	// friendServer := InitializeFriend(db)
+	// friendRequestServer := InitializeFriendRequest(db)
+	// shareServer := InitializeShare(db)
+	// userContentServer := InitializeUserContent(db)
+	// userServer := InitializeUser(db)
 
 	contentServer.RegisterEndpoints(router)
-	domainServer.RegisterEndpoints(router)
-	friendServer.RegisterEndpoints(router)
-	friendRequestServer.RegisterEndpoints(router)
-	shareServer.RegisterEndpoints(router)
-	userContentServer.RegisterEndpoints(router)
-	userServer.RegisterEndpoints(router)
+	// domainServer.RegisterEndpoints(router)
+	// friendServer.RegisterEndpoints(router)
+	// friendRequestServer.RegisterEndpoints(router)
+	// shareServer.RegisterEndpoints(router)
+	// userContentServer.RegisterEndpoints(router)
+	// userServer.RegisterEndpoints(router)
 
 	router.Run(":8000")
 }
--- cmd/later/wire.go
+++ cmd/later/wire.go
@@ -4,81 +4,81 @@ package main
 
 import (
 	"database/sql"
+	"later/pkg/parse"
+	"later/pkg/server"
+	"later/pkg/service"
 
-	"later.co/pkg/parse"
-	"later.co/pkg/repository"
+	"later/pkg/repository"
 
 	"github.com/google/wire"
-	"later.co/pkg/manager"
-	"later.co/pkg/server"
 )
 
-func InitializeContent(db *sql.DB) server.ContentServer {
+func InitializeContent(db *sql.DB) server.Content {
 	wire.Build(
-		server.NewContentServer,
-		manager.NewContentManager,
-		manager.NewDomainManager,
+		server.NewContent,
+		parse.NewContent,
+		service.NewContentManager,
+		service.NewDomainManager,
 		repository.NewContentRepository,
-		repository.NewDomainRepository,
-		parse.NewParser)
-	return server.ContentServer{}
-}
-
-func InitializeDomain(db *sql.DB) server.DomainServer {
-	wire.Build(
-		server.NewDomainServer,
-		manager.NewDomainManager,
 		repository.NewDomainRepository)
-	return server.DomainServer{}
+	return server.Content{}
 }
 
-func InitializeFriend(db *sql.DB) server.FriendServer {
-	wire.Build(
-		server.NewFriendServer,
-		manager.NewFriendManager,
-		manager.NewUserManager,
-		repository.NewFriendRepository,
-		repository.NewUserRepository)
-	return server.FriendServer{}
-}
+// func InitializeDomain(db *sql.DB) server.DomainServer {
+// 	wire.Build(
+// 		server.NewDomainServer,
+// 		service.NewDomainManager,
+// 		repository.NewDomainRepository)
+// 	return server.DomainServer{}
+// }
 
-func InitializeFriendRequest(db *sql.DB) server.FriendRequestServer {
-	wire.Build(
-		server.NewFriendRequestServer,
-		manager.NewFriendRequestManager,
-		repository.NewFriendRequestRepository)
-	return server.FriendRequestServer{}
-}
+// func InitializeFriend(db *sql.DB) server.FriendServer {
+// 	wire.Build(
+// 		server.NewFriendServer,
+// 		service.NewFriendManager,
+// 		service.NewUserManager,
+// 		repository.NewFriendRepository,
+// 		repository.NewUserRepository)
+// 	return server.FriendServer{}
+// }
 
-func InitializeShare(db *sql.DB) server.ShareServer {
-	wire.Build(
-		server.NewShareServer,
-		manager.NewContentManager,
-		manager.NewDomainManager,
-		manager.NewShareManager,
-		manager.NewUserContentManager,
-		manager.NewUserManager,
-		repository.NewContentRepository,
-		repository.NewDomainRepository,
-		repository.NewShareRepository,
-		repository.NewUserContentRepository,
-		repository.NewUserRepository,
-		parse.NewParser)
-	return server.ShareServer{}
-}
+// func InitializeFriendRequest(db *sql.DB) server.FriendRequestServer {
+// 	wire.Build(
+// 		server.NewFriendRequestServer,
+// 		service.NewFriendRequestManager,
+// 		repository.NewFriendRequestRepository)
+// 	return server.FriendRequestServer{}
+// }
 
-func InitializeUserContent(db *sql.DB) server.UserContentServer {
-	wire.Build(
-		server.NewUserContentServer,
-		manager.NewUserContentManager,
-		repository.NewUserContentRepository)
-	return server.UserContentServer{}
-}
+// func InitializeShare(db *sql.DB) server.ShareServer {
+// 	wire.Build(
+// 		server.NewShareServer,
+// 		service.NewContentManager,
+// 		service.NewDomainManager,
+// 		service.NewShareManager,
+// 		service.NewUserContentManager,
+// 		service.NewUserManager,
+// 		repository.NewContentRepository,
+// 		repository.NewDomainRepository,
+// 		repository.NewShareRepository,
+// 		repository.NewUserContentRepository,
+// 		repository.NewUserRepository,
+// 		service.NewParser)
+// 	return server.ShareServer{}
+// }
 
-func InitializeUser(db *sql.DB) server.UserServer {
-	wire.Build(
-		server.NewUserServer,
-		manager.NewUserManager,
-		repository.NewUserRepository)
-	return server.UserServer{}
-}
+// func InitializeUserContent(db *sql.DB) server.UserContentServer {
+// 	wire.Build(
+// 		server.NewUserContentServer,
+// 		service.NewUserContentManager,
+// 		repository.NewUserContentRepository)
+// 	return server.UserContentServer{}
+// }
+
+// func InitializeUser(db *sql.DB) server.UserServer {
+// 	wire.Build(
+// 		server.NewUserServer,
+// 		service.NewUserManager,
+// 		repository.NewUserRepository)
+// 	return server.UserServer{}
+// }
